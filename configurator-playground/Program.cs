34046dcc922d0add5bd8deafb4843963444283ce
rlexa <alexey.rempel@gmail.com>
2017-05-08 00:26:30 +0200

    playground init commit

---
+++ configurator-playground/Program.cs
@@ -0,0 +1,110 @@
+﻿using Configurator;
+using System;
+using System.Collections.Generic;
+
+namespace ConfiguratorTest
+{
+    class Program
+    {
+        public const int EXITCODE_OK = 0;
+        public const int EXITCODE_PATH = 1;
+        public const int EXITCODE_PARSE = 2;
+        public const int EXITCODE_TEST = 3;
+
+        public const string PARAM_CONTEXTPATH = "../../context";
+
+        static void Main(string[] args) {
+            int iExitCode = 0;
+
+            string[] paths = new string[] { PARAM_CONTEXTPATH + ".xml", PARAM_CONTEXTPATH + ".json" };
+            foreach (string path in paths) {
+                if (iExitCode == EXITCODE_OK) {
+                    try {
+                        IContext context = ContextLoader.loadContext(path);
+
+                        try { context.inflate("fail_noparent"); } catch (Exception) { }
+
+                        bool bool_ = (bool)context.inflate("bool");
+                        char char_ = (char)context.inflate("char");
+                        short short_ = (short)context.inflate("short");
+                        int int_ = (int)context.inflate("int");
+                        long long_ = (long)context.inflate("long");
+                        ushort ushort_ = (ushort)context.inflate("ushort");
+                        uint uint_ = (uint)context.inflate("uint");
+                        ulong ulong_ = (ulong)context.inflate("ulong");
+                        float float_ = (float)context.inflate("float");
+                        double double_ = (double)context.inflate("double");
+                        string string_ = (string)context.inflate("string");
+
+                        Int16 int16 = (Int16)context.inflate("int16");
+                        Int32 int32 = (Int32)context.inflate("int32");
+                        Int64 int64 = (Int64)context.inflate("int64");
+
+                        TestContext test = context.inflate("test") as TestContext;
+                        bool bSingle = test.reference_single0 == test.reference_single1;
+                        bool bProto = test.reference_proto0 == test.reference_proto1;
+                        bool bFailed = false;
+                        bool bFailedParent = false;
+                        try {
+                            context.inflate("testParent");
+                        } catch (Exception) { bFailedParent = true; }
+                        try {
+                            TestContext.TestContextInner testImported = context.inflate("testImportedId") as TestContext.TestContextInner;
+                        } catch (Exception) { bFailed = true; }
+                        try {
+                            TestContext.TestContextInner testOverride = context.inflate("testOverride") as TestContext.TestContextInner;
+                        } catch (Exception) { bFailed = true; }
+                        try {
+                            TestContext.TestContextInner testSConstructor1 = context.inflate("testStaticConstructor1") as TestContext.TestContextInner;
+                            bFailed |= testSConstructor1.test != "static";
+                            TestContext.TestContextInner testSConstructor2 = context.inflate("testStaticConstructor2") as TestContext.TestContextInner;
+                            bFailed |= testSConstructor2.test != "static_param";
+                        } catch (Exception) { bFailed = true; }
+                        try {
+                            TestContext.TestContextInner testConstructor = context.inflate("testConstructor") as TestContext.TestContextInner;
+                            bFailed |= testConstructor.test != "constructed";
+                            TestContext.TestContextInner testCConstructor = context.inflate("testCopyConstructor") as TestContext.TestContextInner;
+                            bFailed |= testCConstructor.test != "constructed";
+                        } catch (Exception) { bFailed = true; }
+                        iExitCode = bFailedParent && !bFailed ? EXITCODE_OK : EXITCODE_TEST;
+                    } catch (Exception ex) {
+                        iExitCode = EXITCODE_PARSE;
+                        Console.Error.WriteLine("ERROR: Exception while parsing '" + path + "': {0}", ex);
+                        Console.Error.WriteLine(ex.StackTrace);
+                    }
+                }
+            }
+
+            Environment.Exit(iExitCode);
+        }
+
+        private static void testGenerics() {
+            Type generic = typeof(Dictionary<,>);
+            __DisplayTypeInfo(generic);
+            Type[] typeArgs = { typeof(string), typeof(TestContext) };
+            Type constructed = generic.MakeGenericType(typeArgs);
+            __DisplayTypeInfo(constructed);
+            Console.WriteLine("\r\n--- Compare types obtained by different methods:");
+            Type t = typeof(Dictionary<String, TestContext>);
+            Console.WriteLine("\tAre the constructed types equal? {0}", t == constructed);
+            Console.WriteLine("\tAre the generic types equal? {0}",
+                t.GetGenericTypeDefinition() == generic);
+        }
+
+        private static void __DisplayTypeInfo(Type t) {
+            Console.WriteLine("\r\n{0}", t);
+
+            Console.WriteLine("\tIs this a generic type definition? {0}",
+                t.IsGenericTypeDefinition);
+
+            Console.WriteLine("\tIs it a generic type? {0}",
+                t.IsGenericType);
+
+            Type[] typeArguments = t.GetGenericArguments();
+            Console.WriteLine("\tList type arguments ({0}):", typeArguments.Length);
+            foreach (Type tParam in typeArguments) {
+                Console.WriteLine("\t\t{0}", tParam);
+            }
+        }
+    }
+}
---
+++ configurator-playground/TestContext.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace ConfiguratorTest
+{
+    public class TestContext
+    {
+        public class TestContextInner
+        {
+            public TestContextInner() { }
+            public TestContextInner(string _test) { test = _test; }
+            public TestContextInner(TestContextInner _test) { test = _test.test; }
+            public string test;
+
+            public static TestContextInner GetInstance() {
+                if (s_instance1 == null)
+                    s_instance1 = new TestContextInner("instance");
+                return s_instance1;
+            }
+            public static TestContextInner GetInstance(string param1) {
+                if (s_instance2 == null)
+                    s_instance2 = new TestContextInner(param1);
+                return s_instance2;
+            }
+            private static TestContextInner s_instance1 = null;
+            private static TestContextInner s_instance2 = null;
+        }
+
+        public class TestContextInnerInherited: TestContextInner
+        {
+            public string inherited;
+        }
+
+        public struct TestContextInnerStruct
+        {
+            public int foo;
+        }
+
+        public string string_public;
+        private int int_setter;
+        private float float_property;
+        private string string_nullable = "not_null";
+        private bool bool_fromparent = false;
+        private string string_fromparentoverwrite;
+        public TestContextInner reference_single0 = null;
+        public TestContextInner reference_single1 = null;
+        public TestContextInner reference_proto0 = null;
+        public TestContextInner reference_proto1 = null;
+        public TestContextInner nested = null;
+        public Dictionary<int, string> map_merge = null;
+        public Dictionary<int, string> map = null;
+        public List<double> list_merge = null;
+        public List<double> list = null;
+        public long[] array = null;
+        public HashSet<short> set_merge = null;
+        public HashSet<short> set = null;
+        public Dictionary<string, TestContextInnerStruct> map_struct = null;
+
+        public void setPrivateint(int value) {
+            int_setter = value;
+        }
+
+        public float FloatProperty {
+            get { return float_property; }
+            set { float_property = value; }
+        }
+
+        public void setNullable(string value) {
+            string_nullable = value;
+        }
+
+        public void setFromparent(bool value) {
+            bool_fromparent = value;
+        }
+
+        public void setFromparentoverwrite(string value) {
+            string_fromparentoverwrite = value;
+        }
+    }
+}
